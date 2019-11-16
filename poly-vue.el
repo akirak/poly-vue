3e424b56de55e4eb7545fb85b9f1f70af89cecf5
Akira Komamura <akira.komamura@gmail.com>
2019-11-16 16:28:19 +0900

    Make it fail

--- poly-vue.el
+++ poly-vue.el
@@ -51,29 +51,20 @@
 (defconst poly-vue-script-head-regexp
   (format poly-vue-generic-head-regexp "script"))
 
-(defconst poly-vue-style-head-regexp
-  (rx bol "<style"
-      (* (and space (or "scoped"
-                        "module"
-                        (and (+ (any wordchar))
-                             "="
-                             (any "\"'")
-                             (*? anything)
-                             (any "\"'")))))
-      (and (+ space)
-           "lang="
-           (any "\"'")
-           (group (+ (any alpha)))
-           (any "\"'"))
-      (* (and space (or "scoped"
-                        "module"
-                        (and (+ (any wordchar))
-                             "="
-                             (any "\"'")
-                             (*? anything)
-                             (any "\"'")))))
-      (* space)
-      ">" eol))
+(and (+ space)
+     "lang="
+     (any "\"'")
+     (group (+ (any alpha)))
+     (any "\"'"))
+(* (and space (or "scoped"
+                  "module"
+                  (and (+ (any wordchar))
+                       "="
+                       (any "\"'")
+                       (*? anything)
+                       (any "\"'")))))
+(* space)
+">" eol))
 
 (defcustom poly-vue-template-languages
   '(("html" . vue-html-mode))
